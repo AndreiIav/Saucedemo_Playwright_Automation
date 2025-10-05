c3f242abea5ebe8a466c6d894cb1f00f26267c0d
Your Name <you@example.com>
2025-10-05 12:20:31 +0300

    Updated the function to re-use authentication state to read from .env and to use LoginPage.login()

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -1,21 +1,28 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
+import { LoginPage } from '../pages/LoginPage';
 
 const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
 
-// console.log(authFile)
-
 setup('authenticate', async ({ page }) => {
+    const standardUser = process.env.STANDARD_USER;
+    const password = process.env.PASSWORD;
+
+    if (standardUser === undefined) {
+        throw new Error('STANDARD_USER not defined in .env')
+    } else if (password === undefined) {
+        throw new Error('PASSWORD not defined in .env')
+    }
+
     // Perform authentication steps.
     await page.goto('https://www.saucedemo.com/');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password', 'secret_sauce');
-    await page.click('#login-button');
+    const loginPage = new LoginPage(page);
+    await loginPage.login(standardUser, password);
     // Wait until the page receives the cookies.
     //
     // Sometimes login flow sets cookies in the process of several redirects.
     // Wait for the final URL to ensure that the cookies are actually set.
-    await page.waitForURL('https://www.saucedemo.com/inventory.html', { timeout: 30000 });
+    await page.waitForURL('https://www.saucedemo.com/inventory.html');
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
     await page.context().storageState({ path: authFile })
