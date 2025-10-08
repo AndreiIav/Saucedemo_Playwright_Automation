a24ddc4718f9ef12353289cce63f22d56a0c596c
Your Name <you@example.com>
2025-10-08 08:14:49 +0300

    Updated authenticate to not use env variables for username and password

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -1,23 +1,15 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
 import { LoginPage } from '../pages/LoginPage';
+import users from '../test-data/users';
 
 const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
 
 setup('authenticate', async ({ page }) => {
-    const standardUser = process.env.STANDARD_USER;
-    const password = process.env.PASSWORD;
-
-    if (standardUser === undefined) {
-        throw new Error('STANDARD_USER not defined in .env')
-    } else if (password === undefined) {
-        throw new Error('PASSWORD not defined in .env')
-    }
-
     // Perform authentication steps.
-    await page.goto('https://www.saucedemo.com/');
     const loginPage = new LoginPage(page);
-    await loginPage.login(standardUser, password);
+    await loginPage.goto();
+    await loginPage.login(users.standardUser.username, users.standardUser.password);
     // Wait until the page receives the cookies.
     //
     // Sometimes login flow sets cookies in the process of several redirects.
