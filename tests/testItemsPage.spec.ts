4d78c19a19707bef9f9fecc8f85583c22a4c9339
Your Name <you@example.com>
2025-10-08 08:16:00 +0300

    Replaced 'product' with 'item' everywhere for consistency

--- tests/testItemsPage.spec.ts
+++ tests/testItemsPage.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from '@playwright/test';
 import { InventoryPage } from './pages/InventoryPage'
 
 
-test('Product can be added to cart', async ({ page }) => {
-    const testProductName = 'Sauce Labs Backpack';
+test('Item can be added to cart', async ({ page }) => {
+    const testItemName = 'Sauce Labs Backpack';
     const inventoryPage = new InventoryPage(page);
 
     await inventoryPage.goto();
     //add item to cart
-    const item = await inventoryPage.getOneItemByName(testProductName);
+    const item = await inventoryPage.getOneItemByName(testItemName);
     const itemRemoveButton = await inventoryPage.getItemButton(item, "Remove");
     await inventoryPage.addItemToCart(item);
 
@@ -17,13 +17,13 @@ test('Product can be added to cart', async ({ page }) => {
 
 });
 
-test('Product can be removed from cart', async ({ page }) => {
-    const testProductName = 'Sauce Labs Backpack';
+test('Item can be removed from cart', async ({ page }) => {
+    const testItemName = 'Sauce Labs Backpack';
     const inventoryPage = new InventoryPage(page);
 
     await inventoryPage.goto();
     //add item to cart
-    const item = await inventoryPage.getOneItemByName(testProductName);
+    const item = await inventoryPage.getOneItemByName(testItemName);
     const itemRemoveButton = await inventoryPage.getItemButton(item, 'Remove');
     const itemAddButton = await inventoryPage.getItemButton(item, 'addToCart');
     await inventoryPage.addItemToCart(item);
