d5b8eec111876b648646c120ba96e68dd17d9933
Your Name <you@example.com>
2025-10-23 08:07:38 +0300

    Added CheckoutOnePage POM and tests for Checkout Step One page

---
+++ tests/pages/CheckoutOnePage.ts
@@ -0,0 +1,44 @@
+import { type Page, type Locator } from '@playwright/test';
+
+
+export class CheckoutOnePage {
+    readonly page: Page;
+    readonly firstNameField: Locator;
+    readonly lastNameField: Locator;
+    readonly zipPostalCodeField: Locator;
+    readonly cancelButton: Locator;
+    readonly continueButton: Locator;
+    readonly checkoutError: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.firstNameField = page.getByTestId('firstName');
+        this.lastNameField = page.getByTestId('lastName');
+        this.zipPostalCodeField = page.getByTestId('postalCode');
+        this.cancelButton = page.getByTestId('cancel');
+        this.continueButton = page.getByTestId('continue');
+        this.checkoutError = page.getByTestId('error');
+    }
+
+    async clickCancelButton() {
+        await this.cancelButton.click();
+    }
+
+    async clickContinueButton() {
+        await this.continueButton.click();
+    }
+
+    async enterCheckoutInfo(
+        firstName: string,
+        lastName: string,
+        zipPostalCode: string
+    ) {
+        await this.firstNameField.fill(firstName);
+        await this.lastNameField.fill(lastName);
+        await this.zipPostalCodeField.fill(zipPostalCode);
+    }
+
+    async getErrorText() {
+        return await this.checkoutError.textContent();
+    }
+}
---
+++ tests/testCheckoutOne.spec.ts
@@ -0,0 +1,88 @@
+import { test, expect } from '@playwright/test';
+import { InventoryPage } from './pages/InventoryPage';
+import { HeaderPage } from './pages/HeaderPage';
+import { CartPage } from './pages/CartPage';
+import { CheckoutOnePage } from './pages/CheckoutOnePage';
+
+test('Next page can be accessed after all required information has been filled',
+    async ({ page }) => {
+        const firstName = 'firstName';
+        const lastName = 'lastName';
+        const zipPostalCode = 'zipPostalCode';
+        const checkoutOverviewUrl = 'https://www.saucedemo.com/checkout-step-two.html'
+        const inventoryPage = new InventoryPage(page);
+        const headerPage = new HeaderPage(page);
+        const cartPage = new CartPage(page);
+        const checkoutOnePage = new CheckoutOnePage(page);
+
+        await inventoryPage.goto();
+        await headerPage.clickCartButton();
+        await cartPage.clickCheckoutButton();
+        await checkoutOnePage.enterCheckoutInfo(
+            firstName, lastName, zipPostalCode);
+        await checkoutOnePage.clickContinueButton();
+
+        expect(page).toHaveURL(checkoutOverviewUrl);
+    });
+
+test('Can go back to Cart page', async ({ page }) => {
+    const cartUrl = 'https://www.saucedemo.com/cart.html';
+    const inventoryPage = new InventoryPage(page);
+    const headerPage = new HeaderPage(page);
+    const cartPage = new CartPage(page);
+    const checkoutOnePage = new CheckoutOnePage(page);
+
+    await inventoryPage.goto();
+    await headerPage.clickCartButton();
+    await cartPage.clickCheckoutButton();
+    await checkoutOnePage.clickCancelButton();
+
+    expect(page).toHaveURL(cartUrl);
+});
+
+test.describe('Test form errors', () => {
+
+    [
+        {
+            fieldTest: 'First Name',
+            firstName: '',
+            lastName: 'lastName',
+            zipPostalCode: 'zipPostalCode',
+            expectedErrorMessage: 'Error: First Name is required'
+        },
+        {
+            fieldTest: 'Last Name',
+            firstName: 'firstName',
+            lastName: '',
+            zipPostalCode: 'zipPostalCode',
+            expectedErrorMessage: 'Error: Last Name is required'
+        },
+        {
+            fieldTest: 'Zip/Postal Code',
+            firstName: 'firstName',
+            lastName: 'lastName',
+            zipPostalCode: '',
+            expectedErrorMessage: 'Error: Postal Code is required'
+        },
+
+    ].forEach(({ fieldTest, firstName, lastName, zipPostalCode, expectedErrorMessage }) => {
+        test(`Cannot continue if '${fieldTest}' field is not filled`, async ({ page }) => {
+            const inventoryPage = new InventoryPage(page);
+            const headerPage = new HeaderPage(page);
+            const cartPage = new CartPage(page);
+            const checkoutOnePage = new CheckoutOnePage(page);
+
+            await inventoryPage.goto();
+            await headerPage.clickCartButton();
+            await cartPage.clickCheckoutButton();
+            await checkoutOnePage.enterCheckoutInfo(
+                firstName, lastName, zipPostalCode);
+            await checkoutOnePage.clickContinueButton();
+            const errorText = await checkoutOnePage.getErrorText();
+
+            expect(errorText).toBe(expectedErrorMessage);
+        });
+
+    });
+
+});
