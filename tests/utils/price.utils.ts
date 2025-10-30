868d6bbc0b2b127c2e98bcc3441ced82021201d2
Your Name <you@example.com>
2025-10-30 08:45:31 +0200

    Added price utilities functions in a separate file

---
+++ tests/utils/price.utils.ts
@@ -0,0 +1,15 @@
+export function extractNumberFromString(itemPrice: String) {
+    return Number(itemPrice.replace(/[^.\d]/g, ''));
+}
+
+export function calculateTotal(...args: number[]) {
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum
+}
+
+export function calculateTax(totalItemsPrice: number, tax = 0.08, digits = 2) {
+    const itemsTax = tax * totalItemsPrice;
+    const itemsTaxFixed = itemsTax.toFixed(digits);
+    return extractNumberFromString(itemsTaxFixed);
+}
