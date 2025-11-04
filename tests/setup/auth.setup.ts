c25f580a95139babd200652bde2650e961738508
Your Name <you@example.com>
2025-11-04 08:36:40 +0200

    Updated to work with ESLint rules

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -1,21 +1,24 @@
 import { test as setup, expect } from '@playwright/test';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import { LoginPage } from '../pages/LoginPage';
 import users from '../test-data/users';
 
-const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
+const rootDirectory = fileURLToPath(import.meta.url);
+const rootDirectoryPath = path.dirname(rootDirectory);
+const authFile = path.join(rootDirectoryPath, '../../playwright/.auth/user.json');
 
 setup('authenticate', async ({ page }) => {
-    // Perform authentication steps.
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.login(users.standardUser.username, users.standardUser.password);
-    // Wait until the page receives the cookies.
-    //
-    // Sometimes login flow sets cookies in the process of several redirects.
-    // Wait for the final URL to ensure that the cookies are actually set.
-    await page.waitForURL('https://www.saucedemo.com/inventory.html');
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  // Perform authentication steps.
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login(users.standardUser.username, users.standardUser.password);
+  // Wait until the page receives the cookies.
+  //
+  // Sometimes login flow sets cookies in the process of several redirects.
+  // Wait for the final URL to ensure that the cookies are actually set.
+  await page.waitForURL('https://www.saucedemo.com/inventory.html');
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
-    await page.context().storageState({ path: authFile })
+  await page.context().storageState({ path: authFile });
 });
