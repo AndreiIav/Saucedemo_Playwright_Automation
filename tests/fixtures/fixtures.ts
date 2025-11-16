73d96763fa409de5b9e0c21671940832b87f3064
Your Name <you@example.com>
2025-11-16 10:30:28 +0200

    Added inventoryPage and headerPage fixtures

--- tests/fixtures/fixtures.ts
+++ tests/fixtures/fixtures.ts
@@ -1,10 +1,29 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { InventoryPage } from '../pages/InventoryPage';
+import { HeaderPage } from '../pages/HeaderPage';
 
-export const test = base.extend<{ loginPage: LoginPage }>({
+type MyFixtures = {
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+  headerPage: HeaderPage;
+};
+
+export const test = base.extend<MyFixtures>({
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await use(loginPage);
+  },
+
+  inventoryPage: async ({ page }, use) => {
+    const inventoryPage = new InventoryPage(page);
+    await inventoryPage.goto();
+    await use(inventoryPage);
+  },
+
+  headerPage: async ({ page }, use) => {
+    const headerPage = new HeaderPage(page);
+    await use(headerPage);
   },
 });
